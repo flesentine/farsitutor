75a45105dcbb4eca48afa1b153f6b88837350e23
flesentine <chrisgmcc@gmail.com>
2026-07-12 12:34:15 -0700

    Bump offline cache for runtime coordination fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'farsi-daily-cache-v18';
+const CACHE = 'farsi-daily-cache-v19';
 const ASSETS = [
   './', './index.html', './styles.css', './verb-upgrade.css', './learning-upgrade.css',
   './mobile-experience.css?v=1', './guided-learning.css?v=1', './ux-polish.css?v=1',
